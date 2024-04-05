5ac809c394f037e59605a3f214967d0bd76b3005
Nate Johnson <njohnson2897@gmail.com>
2024-04-04 19:28:34 -0500

    adds get category by id route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -14,9 +14,21 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if(!categoryData) {
+      res.status(404).json({ message: `No category with id of ${req.params.id} found!` });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
