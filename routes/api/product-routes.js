00be75b00fb00ad6b377a982c808bf9cc003fef8
Nate Johnson <njohnson2897@gmail.com>
2024-04-04 19:08:52 -0500

    adds get product by id route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -16,9 +16,21 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category },{ model: Tag }],
+    });
+
+    if(!productData) {
+      res.status(404).json({ message: `No product with id of ${req.params.id} found!` });
+      return;
+    }
+    
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
