7cb07ce5ab8ad68e613951a4ca89728d725dad70
Nate Johnson <njohnson2897@gmail.com>
2024-04-05 09:06:34 -0500

    adds get tag by ID route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -14,9 +14,21 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if(!tagData) {
+      res.status(404).json({ message: `No tag with id of ${req.params.id} found!` });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
