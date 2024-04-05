d7c3c621467c254f368439e74503778518ca15b9
Nate Johnson <njohnson2897@gmail.com>
2024-04-05 08:53:51 -0500

    adds post  category route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -31,16 +31,43 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
-});
+  // req.body should look like this...
+  // {
+  // category_name: "string here"
+  // }
+  try {
+    const categoryData = await Category.create({
+      category_name: req.body.category_name,
+    });
+    res.status(200).json(categoryData);
+    } catch (err) {
+      res.status(400).json(err)
+    }
+  });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  // try {
+  //   const categoryData = await Category.destroy({
+  //     where: {
+  //       id: req.params.id,
+  //     },
+  //   });
+
+  //   if (!categoryData) {
+  //     res.status(404).json({ message: `No category with id of ${req.params.id} found!` });
+  //     return;
+  //   }
+
+  //   res.status(200).json(categoryData);
+  // } catch (err) {
+  //   res.status(500).json(err);
+  // }
 });
 
 module.exports = router;
