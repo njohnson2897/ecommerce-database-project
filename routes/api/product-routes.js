e9b56d01354e2eb105e3e2830dac16e9233d119b
Nate Johnson <njohnson2897@gmail.com>
2024-04-04 19:12:13 -0500

    adds product delete route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -26,7 +26,7 @@ router.get('/:id', async (req, res) => {
       res.status(404).json({ message: `No product with id of ${req.params.id} found!` });
       return;
     }
-    
+
     res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
@@ -110,8 +110,23 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: `No product with id of ${req.params.id} found!` });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
