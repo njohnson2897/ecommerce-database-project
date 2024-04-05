eced8cad6096b7aec22df4bc9fdb12befbf18a83
Nate Johnson <njohnson2897@gmail.com>
2024-04-05 09:23:00 -0500

    adds and debugs tag delete route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -65,7 +65,6 @@ router.post('/', (req, res) => {
     });
 });
 
-// update product
 router.put('/:id', (req, res) => {
   // update product data
   Product.update(req.body, {
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -46,12 +46,46 @@ router.post('/', async (req, res) => {
     }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+   // req.body should look like this...
+  // {
+  // tag_name: "new tag name here"
+  // }
+  // try {
+  //   const tagData = await Tag.update(req.body, {
+  //     where: {
+  //       id: req.params.id,
+  //     },
+  //   })
+    
+  //   if (!tagData) {
+  //     res.status(404).json({ message: `No tag with id of ${req.params.id} found!` });
+  //     return;
+  //   }
+
+  // } catch (err) {
+  //   res.status(500).json(err)
+  // }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+    try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: `No tag with id of ${req.params.id} found!` });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
